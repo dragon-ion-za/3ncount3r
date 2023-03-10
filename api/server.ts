66deecfd6abdef598236fce5dcbf38cbe19fb675
Ian Erasmus <ian.erasmus@entelect.co.za>
2023-03-10 15:26:37 +0200

    #58 - Created api project

---
+++ api/server.ts
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+import { router } from './router';
+
+const port: number = 5002;
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+  
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use('/', router);
+  
+app.listen(port, function() {
+    console.log(`Server started on port ${port}`);
+});
