34641258c66bc63c62da3dda38df176130f6747d
Ian Erasmus <ian.e@live.co.za>
2023-06-13 21:36:35 +0200

    #65 - Added scaffolded menu component

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, MainMenuComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
---
+++ apps/shell/src/app/main-menu/main-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nctr-main-menu',
+  templateUrl: './main-menu.component.html',
+  styleUrls: ['./main-menu.component.css']
+})
+export class MainMenuComponent {}
