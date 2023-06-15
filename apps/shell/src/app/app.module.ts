2ffd7d336aa3e22773d11458cf1ddf42c3e599f0
Ian Erasmus <ian.e@live.co.za>
2023-06-15 20:23:41 +0200

    #65 - Started to get the menu to do what I want, now to start adding the remotes

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -7,14 +7,22 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion'
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, MainMenuComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
     MatSidenavModule,
+    MatListModule,
+    MatExpansionModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
--- apps/shell/src/app/main-menu/main-menu.component.ts
+++ apps/shell/src/app/main-menu/main-menu.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'nctr-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.css']
 })
-export class MainMenuComponent {}
+export class MainMenuComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+}
