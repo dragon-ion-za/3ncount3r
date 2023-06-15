c255801c06e31557e71d9fba948bd794aa7c0578
Ian Erasmus <ian.e@live.co.za>
2023-06-15 20:44:31 +0200

    #65 - Converted to dynamically loading remotes

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
+  /*{
+    path: 'test',
+    loadChildren: () =>
+      loadRemoteModule('test', './Module').then((m) => m.RemoteEntryModule),
+  },*/
   {
     path: '',
     component: NxWelcomeComponent,
