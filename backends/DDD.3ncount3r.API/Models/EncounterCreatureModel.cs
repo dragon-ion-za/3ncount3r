c56247a4ab607a2efc25eb6e525c84bd6f0a40f2
Ian Erasmus <ian.erasmus@entelect.co.za>
2024-12-03 12:39:42 +0200

    Simplified encounter model to be more rule system agnostic

--- backends/DDD.3ncount3r.API/Models/EncounterCreatureModel.cs
+++ backends/DDD.3ncount3r.API/Models/EncounterCreatureModel.cs
@@ -17,22 +17,29 @@ namespace DDD._3ncount3r.API.Models
     [BsonElement("byoapiId")]
     public string ByoapiId { get; set; }
 
-    [BsonElement("hitpointMax")]
-    public int HitpointMax { get; set; }
+    [BsonElement("hitpoints")]
+    public HitpointModel[] Hitpoints { get; set; }
 
-    [BsonElement("currentHitpoints")]
-    public int CurrentHitpoints { get; set; }
-
-    [BsonElement("temporaryHitpoints")]
-    public int TemporaryHitpoints { get; set; }
-
-    [BsonElement("initiative")]
-    public int Initiative { get; set; }
+    [BsonElement("turnOrder")]
+    public int TurnOrder { get; set; }
 
     [BsonElement("isPlayerCharacter")]
     public bool IsPlayerCharacter { get; set; } = false;
 
     [BsonElement("isActive")]
     public bool IsActive { get; set; } = true;
+  }
+
+  [BsonIgnoreExtraElements]
+  public class HitpointModel
+  {
+    [BsonElement("type")]
+    public string Type { get; set; }
+
+    [BsonElement("maximum")]
+    public int Maximum { get; set; }
+
+    [BsonElement("current")]
+    public int Current { get; set; }
   }
 }
