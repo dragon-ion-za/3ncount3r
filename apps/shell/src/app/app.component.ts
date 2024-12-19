84089c13808807a3acdd528cb6bb4707da84001c
Ian Erasmus <ian.erasmus@entelect.co.za>
2024-12-19 13:11:58 +0200

    Added the required API audience to the shell auth

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -10,12 +10,21 @@ import { AuthService } from '@auth0/auth0-angular';
 export class AppComponent {
   title = 'shell';
   currentAppRoot = '';
+  public loggedIn = false;
 
   constructor (public auth: AuthService, private router: Router) {
     this.router.events.subscribe((val) => {
       if (val instanceof NavigationEnd) {
         this.currentAppRoot = val.url;
       }
-  });
+    });
+  }
+
+  public async doLogin() {
+    await this.auth.loginWithRedirect({ authorizationParams: { audience: 'https://api.3ncount3r.co.za' } });
+  }
+
+  public async doLogout() {
+    await this.auth.logout({ logoutParams: { returnTo: document.location.origin } });
   }
 }
